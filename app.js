f38101b05e504480a3f475f918b76cb6dbc6352f
cuevacelis <cuevacelis@hotmail.com>
2022-02-12 17:56:21 -0500

    fix

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const app = express();
 
 app.get("/makers/:name", (req, res) => {
   const getNameParam = req.params.name;
-  const response =
+  const name =
     getNameParam.charAt(0).toLocaleUpperCase() + getNameParam.slice(1);
-  res.send(`<h1>Hola ${response}!</h1>`);
+  res.send(`<h1>Hola ${name}!</h1>`);
 });
 
-app.listen("3000", () => console.log("app run in port 3000"));
+app.listen(3000, () => console.log("Listening on port 3000!"));
